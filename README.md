3520004925affc2dcb43942fbe362d8c046beda1
Gary J <github@accu.ca>
2022-05-19 15:43:52 -0700

    Merge Commits

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Native Animation
+
+# Rotate Animation - Reverse
+
+- Use `index` state to switch toValue from 0-1 and 1-0
--- src/App.js
+++ src/App.js
@@ -1,28 +1,98 @@
-import React from 'react';
-import {StyleSheet, View, Text, SafeAreaView, StatusBar} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {Animated, StyleSheet, View, SafeAreaView, StatusBar, Pressable} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Button from './Button';
 
 const App = function() {
+  const animateValue = useRef(new Animated.Value(0)).current;
+  const [animateStyle, setAnimateStyle] = useState([]);
+  const [index, setIndex] = useState(1);
+
+  const interpolate = animateValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '-360deg']
+  });
+
+  const rotateY = function() {
+    const toValue = index ? 0 : 1;
+    setIndex(index ? 0 : 1);
+    const options = {toValue, duration: 3000, useNativeDriver: false};
+    setAnimateStyle({transform: [{rotateZ: interpolate}]});
+    Animated.timing(animateValue, options).start();
+  };
+
+  const rotateX = function() {
+    const toValue = index ? 0 : 1;
+    setIndex(index ? 0 : 1);
+    const options = {toValue, duration: 3000, useNativeDriver: false};
+    setAnimateStyle({transform: [{rotateX: interpolate}]});
+    Animated.timing(animateValue, options).start();
+  };
+
+  const rotateZ = function() {
+    const toValue = index ? 0 : 1;
+    setIndex(index ? 0 : 1);
+    const options = {toValue, duration: 3000, useNativeDriver: false};
+    setAnimateStyle({transform: [{rotateY: interpolate}]});
+    Animated.timing(animateValue, options).start();
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar />
-      <View>
-        <Text style={styles.text}>Hello React Native</Text>
+
+      <View style={styles.main}>
+        <Animated.View style={[styles.circle, animateStyle]}>
+          <Ionicons name="ios-arrow-up" size={50} color='#EEE' />
+        </Animated.View>
+      </View>
+
+      <View style={styles.footer}>
+        <Button title="Rotate X" style={styles.button} onPress={rotateX} />
+        <Button title="Rotate Z" style={styles.button} onPress={rotateZ} />
+        <Button title="Rotate Y" style={styles.button} onPress={rotateY} />
       </View>
     </SafeAreaView >
   );
 };
-
 const styles = StyleSheet.create({
+  pressable: function({pressed}) {
+    return {opacity: pressed ? 0.7 : 1.0};
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'gold',
+    // borderColor: 'blue',
+    // borderWidth: 4,
   },
-  text: {
-    fontSize: 24,
-    fontWeight: '600',
+  circle: {
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#555',
+  },
+  main: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    flexGrow: 1,
+    // borderColor: 'red',
+    // borderWidth: 2,
+  },
+  footer: {
+    flexDirection: 'row',
+    flexGrow: 0,
+    // borderColor: 'red',
+    // borderWidth: 2,
+  },
+  button: {
+    marginVertical: 5,
+    marginHorizontal: 5,
+    paddingHorizontal: 15,
+    width: 120
   },
 });
-
 export default App;
