3e4d85b04dc3643b99a2edeacdc8ddf5811912f2
Gary J <github@accu.ca>
2022-05-19 14:44:13 -0700

    Merge Commits

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
 import React from 'react';
-import {StyleSheet, View, Text, SafeAreaView, StatusBar} from 'react-native';
+import {StyleSheet, View, SafeAreaView, StatusBar, Pressable} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const App = function() {
+
+  const onPress = function() {
+    console.log("Pressed");
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar />
-      <View>
-        <Text style={styles.text}>Hello React Native</Text>
+      <View style={styles.main}>
+        <View style={styles.circle}>
+          <Pressable style={styles.pressable} onPress={onPress} >
+            <View>
+              <Ionicons name="ios-arrow-up" size={50} color='#EEE' />
+            </View>
+          </Pressable>
+        </View>
+      </View >
+      <View style={styles.footer}>
       </View>
     </SafeAreaView >
   );
 };
 
+
 const styles = StyleSheet.create({
+  pressable: function({pressed}) {
+    return [{opacity: pressed ? 0.7 : 1.0}];
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'gold',
   },
-  text: {
-    fontSize: 24,
-    fontWeight: '600',
+  circle: {
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#555',
+    // borderWidth: 4,
+    // borderColor: "red",
+  },
+  main: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    flexGrow: 1,
+    // borderWidth: 4,
+    // borderColor: "red",
+  },
+  footer: {
+    flexDirection: 'row',
+    flexGrow: 0,
   },
 });
+
 
 export default App;
